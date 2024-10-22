8b79639b2d09961533af5116cdf7068aa2773be2
tikandev <verneri@tikan.dev>
2024-10-22 21:48:51 +0300

    Main task name change and description added

--- tasks.py
+++ tasks.py
@@ -1,6 +1,11 @@
 from robocorp.tasks import task
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def order_robots_from_RobotSpareBin():
+    """
+    Orders robots from RobotSpareBin Industries Inc.
+    Saves the order HTML receipt as a PDF file.
+    Saves the screenshot of the ordered robot.
+    Embeds the screenshot of the robot to the PDF receipt.
+    Creates ZIP archive of the receipts and the images.
+    """
