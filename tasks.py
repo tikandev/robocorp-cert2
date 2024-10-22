6c3a516a86c133b2ef9050612bc71ad4ba13ef41
tikandev <verneri@tikan.dev>
2024-10-22 22:27:31 +0300

    Added a function for downloading orders.csv and saving it in output folder

--- tasks.py
+++ tasks.py
@@ -1,6 +1,9 @@
 from robocorp.tasks import task
 from robocorp import browser
 
+from RPA.HTTP import HTTP
+from RPA.Tables import Tables
+
 @task
 def order_robots_from_RobotSpareBin():
     """
@@ -15,7 +18,15 @@ def order_robots_from_RobotSpareBin():
         slowmo=100,
     )
     open_robot_order_website()
+    orders = get_orders()
 
 def open_robot_order_website():
     # Getting the robot to open the correct website
     browser.goto("https://robotsparebinindustries.com/#/robot-order")
+
+def get_orders():
+    # Downloading the .csv file containing the order information, overwriting enabled
+    http = HTTP()
+    http.download(url="https://robotsparebinindustries.com/orders.csv", target_file="output/orders.csv", overwrite=True)
+    ordertable = Tables().read_table_from_csv("output/orders.csv", columns=["Order number", "Head", "Body", "Legs", "Address"])
+    return ordertable
