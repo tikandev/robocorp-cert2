a7bcfd06e774cb9340f53431da829fc9ae871e43
tikandev <verneri@tikan.dev>
2024-10-22 22:04:09 +0300

    Added function to open the robot order website

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from robocorp.tasks import task
+from robocorp import browser
 
 @task
 def order_robots_from_RobotSpareBin():
@@ -9,3 +10,12 @@ def order_robots_from_RobotSpareBin():
     Embeds the screenshot of the robot to the PDF receipt.
     Creates ZIP archive of the receipts and the images.
     """
+    # Adding a slowmo effect to the robot so viewing test results becomes easier
+    browser.configure(
+        slowmo=100,
+    )
+    open_robot_order_website()
+
+def open_robot_order_website():
+    # Getting the robot to open the correct website
+    browser.goto("https://robotsparebinindustries.com/#/robot-order")
